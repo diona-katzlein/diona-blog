9725d48e1658768fcc8733650f4b5481972da820
Diona Katzlein <me@isekai.id>
2026-06-15 12:56:27 +0800

    fix: resolve dark theme page background bug on article view and bypass CDN cache

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -47,15 +47,4 @@
       }
     }
   }
-
-  function updateHighlightTheme(themeMode) {
-    const highlightLink = document.getElementById('highlightTheme');
-    if (highlightLink) {
-      if (themeMode === 'dark') {
-        highlightLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css';
-      } else {
-        highlightLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css';
-      }
-    }
-  }
 })();
